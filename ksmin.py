b74efdc8bc0787b09b1acfa3fa48ebc6b2a16ca9
Michael Leonard <maybeillrememberit@gmail.com>
2019-05-29 22:34:12 -0500

    Start developing a minification tool

---
+++ ksmin.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+"""A minification tool for KerboScript"""
+
+
+def min_strip_comments():
+    """Remove comments from the script"""
+    pass
+
+
+if __name__ == '__main__':
+    pass
